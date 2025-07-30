90a7f3db5b8632c98218479509c9ff08aab714e0
ShanafKhan <shanafkhan4@gmail.com>
2025-07-30 15:43:43 +0530

    Updated the upvote and downvote function using useActionState hook to disable the button based on the pending state

--- src/components/Opinion.jsx
+++ src/components/Opinion.jsx
@@ -1,14 +1,20 @@
 import { use, useActionState } from "react";
 import { OpinionsContext } from "../store/opinions-context";
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
-  const {upvoteOpinion, downvoteOpinion} =use(OpinionsContext);
-  async function upvoteAction(){
+  const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
+  async function upvoteAction() {
     await upvoteOpinion(id);
   }
 
-  async function downvoteAction(){
+  async function downvoteAction() {
     await downvoteOpinion(id);
   }
+
+  const [upvoteFormState, upvoteFormAction, upvotePending] =
+    useActionState(upvoteAction);
+  const [downvoteFormState, downvoteFormAction, downvotePending] =
+    useActionState(downvoteAction);
+
   return (
     <article>
       <header>
@@ -16,8 +22,11 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
         <p>Shared by {userName}</p>
       </header>
       <p>{body}</p>
-      <form className="votes"  >
-        <button formAction={upvoteAction}>
+      <form className="votes">
+        <button
+          formAction={upvoteFormAction}
+          disabled={upvotePending || downvotePending}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -37,7 +46,10 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
         <span>{votes}</span>
 
-        <button formAction={downvoteAction}>
+        <button
+          formAction={downvoteFormAction}
+          disabled={upvotePending || downvotePending}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
